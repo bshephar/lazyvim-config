5c9eb04b8c010e21199b18e6493eefc4fc7b1dd2
Brendan Shephard <bshephar@redhat.com>
2023-11-22 16:24:13 +1000

    Add python dap and dap-go
    Signed-off-by: Brendan Shephard <bshephar@redhat.com>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,4 +1,5 @@
 return {
+  "leoluz/nvim-dap-go",
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
@@ -67,5 +68,17 @@ return {
         -- end workaround
       end,
     },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+	 -- stylua: ignore
+ 	 keys = {
+ 	   { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+ 	   { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+ 	 },
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      require("dap-python").setup(path .. "/venv/bin/python")
+    end,
   },
 }
